d725510cf17f951b3d31909caa07cf4957aff7b2
Luca Naterop <luca.naterop@bluewin.ch>
2017-04-20 11:31:48 +0200

    step 4: display acceleration

--- client/main.js
+++ client/main.js
@@ -1,1 +1,16 @@
 // our code goes here
+
+var acc;
+$(function(){
+	window.addEventListener('devicemotion', function(event) {
+		acc = event.acceleration;
+	});
+	setInterval(() => {
+		$('#accX').html('a_x = ' + Math.round(100*acc.x)/100.0 + ' m/s^2')
+		$('#accY').html('a_y = ' + Math.round(100*acc.y)/100.0 + ' m/s^2')
+		$('#accZ').html('a_z = ' + Math.round(100*acc.z)/100.0 + ' m/s^2')
+		
+	}, 100);
+
+});
+
