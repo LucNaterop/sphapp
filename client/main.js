2d6ecced658b94353527d35215525584b18e4e77
Luca Naterop <luca.naterop@bluewin.ch>
2017-04-20 11:56:59 +0200

    step 6: start and stop buttons

--- client/main.js
+++ client/main.js
@@ -1,16 +1,24 @@
 // our code goes here
 
 var acc;
+
 $(function(){
 	window.addEventListener('devicemotion', function(event) {
 		acc = event.acceleration;
 	});
 	setInterval(() => {
-		$('#accX').html('a_x = ' + Math.round(100*acc.x)/100.0 + ' m/s^2')
-		$('#accY').html('a_y = ' + Math.round(100*acc.y)/100.0 + ' m/s^2')
-		$('#accZ').html('a_z = ' + Math.round(100*acc.z)/100.0 + ' m/s^2')
-		
+		$('#accX').html('a_x = ' + Math.round(100*acc.x)/100.0 + ' m/s^2');
+		$('#accY').html('a_y = ' + Math.round(100*acc.y)/100.0 + ' m/s^2');
+		$('#accZ').html('a_z = ' + Math.round(100*acc.z)/100.0 + ' m/s^2');
 	}, 100);
 
+
+	$('#start').click(() => {
+		alert('you clicked on START');
+	});
+
+	$('#stop').click(() => {
+		alert('you clicked on STOP');
+	})
 });
 
